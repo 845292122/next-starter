1aac15c60cc1ff1c2223ecd6d0794aeb0a4c0798
Edison.Cheung <845292122@qq.com>
2026-06-04 21:46:49 +0800

    集成prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  useTabs: false,
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  semi: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss']
+}
