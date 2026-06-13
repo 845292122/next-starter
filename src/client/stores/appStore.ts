90f13281fa399be50071911fcdc27674ee583561
Edison.Cheung <845292122@qq.com>
2026-06-13 12:58:37 +0800

    zustand

---
+++ src/client/stores/appStore.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface AppState {
+  theme: 'light' | 'dark'
+  toggleTheme: () => void
+}
+
+export const useAppStore = create<AppState>()(
+  persist(
+    set => ({
+      theme: 'light',
+      toggleTheme: () => set(state => ({ theme: state.theme === 'light' ? 'dark' : 'light' }))
+    }),
+    {
+      name: 'app-store' // 存储在 localStorage 中的键名
+    }
+  )
+)
